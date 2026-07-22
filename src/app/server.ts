094ef8e3bd9282ff7d9cd00b00c7de0863cf5986
PedroOpossum <mason.s.morales2005@gmail.com>
2026-07-22 11:20:58 -0500

    Added logo

---
+++ src/app/server.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://uleoeytvajqvikvsksok.supabase.co',
+  'sb_publishable_tNJyRTDqB3EfGhpkqGyR_w_Z65tcyHC'
+);
