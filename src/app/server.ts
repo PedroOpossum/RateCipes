3822bd0a6f91f81a7d64e4c323543bd89e55c457
PedroOpossum <mason.s.morales2005@gmail.com>
2026-08-12 19:33:47 -0500

    Switched the framework to React

--- src/app/server.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://uleoeytvajqvikvsksok.supabase.co',
-  'sb_publishable_tNJyRTDqB3EfGhpkqGyR_w_Z65tcyHC'
-);
